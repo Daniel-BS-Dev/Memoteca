dbce3e6e28e4e67f24d06dc0b9943c0b0633196e
Daniel Silva <daniel.silva@onlineapp.com.br>
2022-11-15 17:10:58 -0300

    show error get all thinking ngrx

--- menoteca/src/app/thinkings/redux/effects.ts
+++ menoteca/src/app/thinkings/redux/effects.ts
@@ -1,13 +1,14 @@
 import { ThinkingModel } from './../../models/thinking.model';
 import { Injectable } from "@angular/core";
 import { Actions, createEffect, ofType } from "@ngrx/effects";
-import { catchError, exhaustMap, map, of, switchMap } from "rxjs";
+import { catchError, exhaustMap, map, of, switchMap, tap } from "rxjs";
 import { ThinkingService } from "../service/thinking.service";
 import * as formThinkingTypeActions from "./action";
+import { ToastrService } from 'ngx-toastr';
 
 @Injectable()
 export class ThinkingEffects {
-  constructor(private action$: Actions, private service: ThinkingService) { }
+  constructor(private action$: Actions, private service: ThinkingService, private toastr: ToastrService) { }
 
   loadThikings$ = createEffect((): any =>
     this.action$.pipe(
@@ -17,10 +18,20 @@ export class ThinkingEffects {
           map((payload: ThinkingModel[]) =>
             formThinkingTypeActions.LoadThinkingsSuccess({ payload })
           ),
-          catchError((error) => of(formThinkingTypeActions.LoadThinkingsFail({ error: 'Ocorreu um erro' })))
+          catchError(() => of(formThinkingTypeActions.LoadThinkingsFail({ error: 'Ocorreu um erro' })))
         )
       )
     )
+  );
+
+  errorLoadThinkings$ = createEffect(() =>
+    this.action$.pipe(
+      ofType(formThinkingTypeActions.thinkingTypeAction.LOAD_THINKINGS_FAIL),
+      tap((error: any) => {
+        this.toastr.error(error.error);
+      })),
+
+    { dispatch: false }
   );
 
 }
